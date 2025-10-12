5868630fd2b950bec91d1d17d0411a35e11654eb
Seyun Sim <tlatpdbs021104@gmail.com>
2025-10-12 12:16:10 +0900

    1012

--- seyun/train_conditional_diffusion.py
+++ seyun/train_conditional_diffusion.py
@@ -140,8 +140,6 @@ def main():
             hidden_size = unet.config.block_out_channels[block_id]
         
         lora_attn_procs[name] = LoRAAttnProcessor(
-            hidden_size=hidden_size, 
-            cross_attention_dim=cross_attention_dim,
             rank=args.lora_rank
         )
     unet.set_attn_processor(lora_attn_procs)
